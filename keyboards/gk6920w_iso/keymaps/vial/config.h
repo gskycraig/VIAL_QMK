7a60308507882e19a420d70c3572b6d637aef1b9
GSKY-Vivobook\GSKY <craig@gsky.com.tw>
2025-02-25 10:40:58 +0800

    NEW

---
+++ keyboards/gk6920w_iso/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x55, 0xB5, 0x4C, 0x61, 0xA0, 0x01, 0x2E, 0x26}
+#define VIAL_UNLOCK_COMBO_ROWS {10, 0}
+#define VIAL_UNLOCK_COMBO_COLS {10, 6}
