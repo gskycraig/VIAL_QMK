595329d228916bbf7b8170fe0310c59710dfd2c0
gskycraig <103167160+gskycraig@users.noreply.github.com>
2026-04-13 09:27:39 +0800

    Add files via upload

---
+++ keyboards/nk68cncwirelessblack/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4E, 0xAD, 0xB8, 0x7E, 0xE5, 0x40, 0xDA, 0xFC}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 6}
